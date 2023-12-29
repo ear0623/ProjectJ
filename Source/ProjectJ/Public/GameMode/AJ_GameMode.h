23825cd4a47279f095edc01b15acd396213650a0
NahYoungLyroal <ear062311@gamil.com>
2023-12-29 17:21:48 +0900

    초기 설정

---
+++ Source/ProjectJ/Public/GameMode/AJ_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AJ_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API AAJ_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
