e0ecb469da127e908e74dd522ace3425af42cd49
NahYoungLyroal <ear062311@gamil.com>
2024-01-02 19:49:15 +0900

    UserWidet c++ 생성

---
+++ Source/ProjectJ/Public/Widget/SStatWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "SStatWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API USStatWidget : public UWidget
+{
+	GENERATED_BODY()
+	
+};
