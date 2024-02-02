c1c79070f6db05365c4dc153c3f8b323e57cb638
NahYoungLyroal <ear062311@gamil.com>
2024-02-02 17:48:15 +0900

    20240202 widget test및 connector

---
+++ Source/ProjectJ/Public/Widget/Lobby/ChatUserWidget.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChatUserWidget.generated.h"
+
+
+class UTextBlock;
+class UScrollBox;
+class UEditableTextBox;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API UChatUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	/*UPROPERTY(BlueprintReadOnly, EditAnywhere,Category ="UI")
+	TObjectPtr<UTextBlock> ChatBox;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "UI")
+	TObjectPtr<UEditableTextBox> ChatMultiBox*/
+
+};
